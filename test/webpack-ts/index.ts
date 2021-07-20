afd9135539cfab9efd0165f394ab5289e98410bc
Lukas Wagner <lukas.wagner@hpi.de>
2021-07-20 14:46:50 +0200

    parse bigger datasets, more stats

--- test/webpack-ts/index.ts
+++ test/webpack-ts/index.ts
@@ -7,7 +7,7 @@ import {
     CsvLoaderOptions,
     DataType,
     LoadStatistics,
-    isNumber
+    isNumber,
 } from '../..';
 import { NumberColumn } from '../../lib/types/types/column/numberColumn';
 
@@ -18,61 +18,87 @@ const options = new CsvLoaderOptions({
     delimiter: ','
 });
 
+function onOpened(this: CSV, tag: string, columns: ColumnHeader[]): void {
+    console.log(
+        tag,
+        `opened source, detected ${columns.length} columns:\n` +
+        columns.map((c) => `${c.name}: ${DataType[c.type]}`).join('\n'));
+    this.load({
+        columns: columns.map((c) => c.type),
+        generatedColumns: []
+    });
+}
+
+type Stats = {
+    tag: string,
+    rows: number,
+    kB: number,
+    time: number,
+    rowsPerSecond: number,
+    kBPerSecond: number
+}
+const statistics = new Array<Stats>();
+
+function onDone(
+    this: CSV, tag: string, done: () => void,
+    columns: Column[], stats: LoadStatistics
+): void {
+    let columnsStats = '=== column stats: ===\n';
+    columnsStats += columns.map((c) => {
+        let text = `${c.name}: ${c.length} rows`;
+        const asNum = c as NumberColumn;
+        if(isNumber(c.type)) text += `, min ${asNum.min}, max ${asNum.max}`;
+        return text;
+    }).join('\n');
+
+    const timeMS =
+        stats.performance.find((s) => s.label === 'open').delta +
+        stats.performance.find((s) => s.label === 'load').delta;
+    const timeS = timeMS / 1000;
+    const kB = stats.bytes / 1000;
+    const rows = columns[0].length;
+    const kRows = rows / 1000;
+
+    statistics.push({
+        tag,
+        rows,
+        kB,
+        time: timeS,
+        rowsPerSecond: rows / timeS,
+        kBPerSecond: kB /timeS
+    });
+
+    const loaderStats =
+        '=== loader stats: ===\n' +
+        `source bytes: ${stats.bytes}\n` +
+        `source chunks: ${stats.chunks}\n` +
+        `number of workers: ${stats.workers}\n` +
+        `read rows: ${rows}\n` +
+        `kB / worker: ${(kB / stats.workers).toFixed(3)}\n` +
+        `chunks / worker: ${(stats.chunks / stats.workers).toFixed(3)}\n` +
+        `total time in s: ${timeS.toFixed(3)}\n` +
+        `kB / s: ${(kB / timeS).toFixed(3)}\n` +
+        `kRows / s: ${(kRows / timeS).toFixed(3)}\n`;
+
+    console.log(tag, `done.\n${columnsStats}\n${loaderStats}`);
+
+    console.groupCollapsed('=== performance stats: ===');
+    stats.performance.forEach(
+        (m) => console.log(`${m.label}: ${m.delta} ms`));
+    console.groupEnd();
+
+    done();
+}
+
 function createLoader(tag: string, done: () => void): CSV {
     const loader = new CSV(options);
-    let storedColumns: Column[];
-    loader.on('opened', (columns: ColumnHeader[]) => {
-        console.log(
-            tag,
-            `opened source, detected ${columns.length} columns:\n` +
-            columns.map((c) => `${c.name}: ${DataType[c.type]}`).join('\n'));
-        loader.load({
-            columns: columns.map((c) => c.type),
-            generatedColumns: []
-        });
-    });
+    loader.on('opened',  onOpened.bind(loader, tag));
     loader.on('columns', (columns: Column[]) => {
-        storedColumns = columns;
         console.log(tag, 'received columns');
+        loader.on('done', onDone.bind(loader, tag, done, columns));
     });
     loader.on('data', (progress: number) => {
         console.log(tag, `received new data. progress: ${progress}`);
-    });
-    loader.on('done', (stats: LoadStatistics) => {
-        const columnsStats =
-            '=== column stats: ===\n' +
-            storedColumns.map((c) => {
-                const base = `${c.name}: ${c.length} rows`;
-                const asNum = c as NumberColumn;
-                const num = isNumber(c.type) ?
-                    `, min ${asNum.min}, max ${asNum.max}` : '';
-                return base + num;
-            }).join('\n');
-        const timeMS =
-            stats.performance.find((s) => s.label === 'open').delta +
-            stats.performance.find((s) => s.label === 'load').delta;
-        const timeS = timeMS / 1000;
-        const kb = stats.bytes / 1000;
-        const kRows = storedColumns[0].length / 1000;
-        const loaderStats =
-            '=== loader stats: ===\n' +
-            `source bytes: ${stats.bytes}\n` +
-            `source chunks: ${stats.chunks}\n` +
-            `number of workers: ${stats.workers}\n` +
-            `read rows: ${storedColumns[0].length}\n` +
-            `kB / worker: ${(kb / stats.workers).toFixed(3)}\n` +
-            `chunks / worker: ${(stats.chunks / stats.workers).toFixed(3)}\n` +
-            `total time in s: ${timeS.toFixed(3)}\n` +
-            `kB / s: ${(kb / timeS).toFixed(3)}\n` +
-            `kRows / s: ${(kRows / timeS).toFixed(3)}\n`;
-        console.log(
-            tag,
-            `done.\n${columnsStats}\n${loaderStats}`);
-        console.groupCollapsed('=== performance stats: ===');
-        stats.performance.forEach(
-            (m) => console.log(`${m.label}: ${m.delta} ms`));
-        console.groupEnd();
-        done();
     });
     loader.on('error', (msg: string) => {
         console.log(tag, 'error:', msg);
@@ -98,5 +124,9 @@ function testGzipped(testCase: string, url: string): Promise<void> {
     });
 }
 
-testUrl('[url stream]', conf.url)
-    .then(() => testGzipped('[1m gzip buffer]', '/data/1m.csv.gz'));
+testUrl('[remote url stream]', conf.url)
+    .then(() => testGzipped('[1m gzip buffer]', require('1m.csv.gz')))
+    .then(() => testUrl('[10m url stream]', require('10m.csv')))
+    .then(() => testUrl('[50m url stream]', require('50m.csv')))
+    .then(() => testUrl('[100m url stream]', require('100m.csv')))
+    .then(() => console.table(statistics));
--- test/webpack-ts/webpack.config.js
+++ test/webpack-ts/webpack.config.js
@@ -1,7 +1,7 @@
 'use strict';
 
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = function () {
     return {
@@ -11,20 +11,28 @@ module.exports = function () {
             rules: [
                 { test: /\.pug$/, use: { loader: 'pug-loader' } },
                 { test: /\.ts$/, use: { loader: 'ts-loader' } },
+                { test: /\.gz$/, type: 'asset/resource' },
+                { test: /\.csv$/, type: 'asset/resource' },
             ],
         },
         resolve: {
             extensions: ['.ts', '.js', '.json'],
+            alias: {
+                '1m.csv.gz': path.resolve(__dirname, '../data/1m.csv.gz'),
+                '10m.csv': path.resolve(__dirname, '../data/10m.csv'),
+                '50m.csv': path.resolve(__dirname, '../data/50m.csv'),
+                '100m.csv': path.resolve(__dirname, '../data/100m.csv'),
+            },
+            fallback: {
+                '10m.csv': path.resolve(__dirname, '../data/fallback.csv'),
+                '50m.csv': path.resolve(__dirname, '../data/fallback.csv'),
+                '100m.csv': path.resolve(__dirname, '../data/fallback.csv'),
+            },
         },
         plugins: [
             new HtmlWebpackPlugin({
                 filename: 'index.html',
                 template: './index.pug'
-            }),
-            new CopyWebpackPlugin({
-                patterns: [
-                    { from: '../data', to: 'data' },
-                ]
             }),
         ],
         devServer: {
