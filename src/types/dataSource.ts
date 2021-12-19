0f85fa095e08fc10fef3a932562ba7756ef4c0d2
Florian Fregien <florian.fregien@student.hpi.uni-potsdam.de>
2021-12-19 19:15:30 +0100

    fix: fix merge issue
    (forgot to commit)

--- src/types/dataSource.ts
+++ src/types/dataSource.ts
@@ -1,3 +1,12 @@
-export type InputData = Blob | File | ArrayBufferLike | Uint8Array | ReadableStream | string;
+export type SheetInput = { apiKey: string; sheetId: string };
+
+export type InputData =
+    | Blob
+    | File
+    | ArrayBufferLike
+    | Uint8Array
+    | ReadableStream
+    | string
+    | SheetInput;
 
 export type DataSource = InputData | Promise<InputData> | (() => Promise<InputData>);
